26709e58271ec3bb607194954eefd132146748f8
Marcos Couto <marcos_otuoc@hotmail.com>
2020-02-27 18:32:28 -0300

    First commit

---
+++ src/main/java/com/marcoscouto/crudmongo/CrudmongoApplication.java
@@ -0,0 +1,13 @@
+package com.marcoscouto.crudmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudmongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudmongoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marcoscouto/crudmongo/CrudmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcoscouto.crudmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
